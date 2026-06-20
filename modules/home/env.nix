023f3a06a0918398381146b2ea65a5a9ad337522
valjean <valjean.hao@gmail.com>
2026-06-20 21:53:56 +0800

    fix typo

--- modules/home/env.nix
+++ modules/home/env.nix
@@ -2,7 +2,7 @@
 {
   home.sessionVariables = {
     LANG = "en_US.UTF-8";
-    LC_ALL = "en_US:UTF-8";
+    LC_ALL = "en_US.UTF-8";
     LANGUAGE = "en_US:en";
     EDITOR = "vim";
     VIUAL = "vim";
