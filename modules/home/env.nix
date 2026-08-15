c268cf6801f19e36d7ba9339db8043a2686ca25a
valjean <valjean.hao@gmail.com>
2026-08-15 14:59:34 +0800

    add environment

--- modules/home/env.nix
+++ modules/home/env.nix
@@ -20,7 +20,15 @@
   };
 
   home.sessionVariables = {
-    # EDITOR = "emacs";
+
+    PAGER='less'
+    LC_TIME=en_US.UTF-8
+    LC_ALL=en_US.UTF-8
+    LC_CTYPE=UTF-8
+    GNUPGHOME=~/.gnupg
+    LANG=en_US.UTF-8
+    EDITOR='vim'
+    VISUAL='vim'
     TERM = "xterm-256color";
   };
 
